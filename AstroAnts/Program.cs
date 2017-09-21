d21165b4cd09bd98159e3f60e9b451b2359b542d
Martin Tomas <m.tomas@gmc.net>
2017-09-21 11:26:37 +0200

    Add project files.

---
+++ AstroAnts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AstroAnts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
